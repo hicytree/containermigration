9d2246dd91067a1a0e340e38420b695fee21e3f5
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2024-05-03 11:50:54 -0700

    Created the frontend application

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 let counter = 0;
 
@@ -12,6 +15,6 @@ setInterval(() => {
   counter++;
 }, 1000);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });

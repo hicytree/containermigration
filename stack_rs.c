3506a01e76621bf0313a254add5a4ad220de68c7
Will-Hsu <46422881+Will-Hsu@users.noreply.github.com>
2024-05-21 11:13:25 -0700

    update rs

--- stack_rs.c
+++ stack_rs.c
@@ -1,111 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
-#include <unistd.h>
 #include <sys/user.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define STACK_IDENTIFIER "[stack]"
+int main() {
+    pid_t child = fork();
+    if (child == 0) {
+        // Child process: stop itself for ptrace attachment
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        // Load and execute the script or executable here
+        execl("testlooper.sh", "testlooper.sh", (char *)NULL);
+    } else {
+        // Parent process: wait for child to stop
+        waitpid(child, NULL, 0);
 
-long find_stack_address(pid_t pid) {
-    char filename[100];
-    sprintf(filename, "/proc/%d/maps", pid);
-    FILE *maps_file = fopen(filename, "r");
-    if (!maps_file) {
-        perror("fopen");
-        return -1;
-    }
+        // Attach to the child process
+        ptrace(PTRACE_ATTACH, child, NULL, NULL);
+        waitpid(child, NULL, 0);
 
-    char line[256];
-    long stack_start = 0;
+        // Modify registers and stack here
+        struct user_regs_struct regs;
+        ptrace(PTRACE_GETREGS, child, NULL, &regs);
 
-    while (fgets(line, sizeof(line), maps_file)) {
-        if (strstr(line, STACK_IDENTIFIER)) {
-            sscanf(line, "%lx-", &stack_start);
-            break;
+        // Assume you have the stack pointer and instruction pointer from the dump
+        regs.rsp = new_stack_pointer_from_dump;
+        regs.rip = new_instruction_pointer_from_dump;
+        
+        // Restore stack content (example, highly simplified)
+        for (int i = 0; i < stack_size; i += sizeof(long)) {
+            long data = *(long *)(stack_dump + i);
+            ptrace(PTRACE_POKEDATA, child, (void *)(new_stack_pointer_from_dump + i), data);
         }
+
+        // Apply the modified registers
+        ptrace(PTRACE_SETREGS, child, NULL, &regs);
+
+        // Continue the process
+        ptrace(PTRACE_DETACH, child, NULL, NULL);
+        waitpid(child, NULL, 0);
     }
-
-    fclose(maps_file);
-
-    return stack_start;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <pid> <dump_file>\n", argv[0]);
-        return 1;
-    }
-
-    pid_t pid = atoi(argv[1]);
-    char *dump_file = argv[2];
-
-    FILE *input = fopen(dump_file, "rb");
-    if (!input) {
-        perror("fopen");
-        return 1;
-    }
-
-    // Get the size of the dump file
-    fseek(input, 0, SEEK_END);
-    size_t dump_size = ftell(input);
-    fprintf(stderr, "Dump size: %ld\n", dump_size);
-    rewind(input);
-
-    // Allocate memory to hold the dump contents
-    char *dump_data = (char *)malloc(dump_size);
-    if (!dump_data) {
-        perror("malloc");
-        fclose(input);
-        return 1;
-    }
-
-    // Read the contents of the dump file into memory (dump_data)
-    if (fread(dump_data, 1, dump_size, input) != dump_size) {
-        perror("fread");
-        fclose(input);
-        free(dump_data);
-        return 1;
-    }
-
-    fclose(input);
-
-    // Attach to the target process
-    if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) < 0) {
-        perror("ptrace attach");
-        free(dump_data);
-        return 1;
-    }
-
-    int status;
-    // 
-    waitpid(pid, &status, 0);
-
-    long stack_start = find_stack_address(pid);
-    if (stack_start == -1) {
-        fprintf(stderr, "Failed to find stack address.\n");
-        free(dump_data);
-        ptrace(PTRACE_DETACH, pid, NULL, NULL);
-        return 1;
-    }
-
-    // Write the contents of the dump file to the stack memory of the target process
-    unsigned long addr;
-    for (addr = stack_start; addr < stack_start + dump_size; addr += sizeof(long)) {
-        long data = *(long *)(dump_data + (addr - stack_start));
-        if (ptrace(PTRACE_POKEDATA, pid, addr, data) < 0) {
-            perror("ptrace poke");
-            break;
-        }
-    }
-
-    // Detach from the target process
-    ptrace(PTRACE_DETACH, pid, NULL, NULL);
-
-    // Clean up
-    free(dump_data);
-
     return 0;
 }
-

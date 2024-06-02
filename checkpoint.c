7266ab29122604dc4d2b1522eb500616c65e4474
Jeffrey Liu <yjeffliu@gmail.com>
2024-06-02 03:12:17 -0700

    debugging registers

--- checkpoint.c
+++ checkpoint.c
@@ -59,7 +59,7 @@ void store_memory(pid_t pid) {
         printf("start_addr: %lx - end_addr: %lx\n", start_addr, end_addr);
 
         // If successfully read from the map, read from the virtual memory addresses found
-        if (ret > 0 && strstr(name, "[stack]")) {    
+        if (ret > 0) {    
             int data_size = end_addr - start_addr;
             char *buffer = malloc(data_size);
 
@@ -107,13 +107,44 @@ void store_memory(pid_t pid) {
         else if (ret == EOF) {
             break;
         }
-        // else {
-        //     printf("Failed: Parsing error.\n");
-        // }
+        else {
+            printf("Failed: Parsing error.\n");
+        }
     }
     fclose(maps_file);
     fclose(fd_m);
     fclose(fd_l);
+}
+
+void print_registers(struct user_regs_struct *regs) {
+    printf("Registers:\n");
+    printf("R15: 0x%llx\n", regs->r15);
+    printf("R14: 0x%llx\n", regs->r14);
+    printf("R13: 0x%llx\n", regs->r13);
+    printf("R12: 0x%llx\n", regs->r12);
+    printf("RBP: 0x%llx\n", regs->rbp);
+    printf("RBX: 0x%llx\n", regs->rbx);
+    printf("R11: 0x%llx\n", regs->r11);
+    printf("R10: 0x%llx\n", regs->r10);
+    printf("R9: 0x%llx\n", regs->r9);
+    printf("R8: 0x%llx\n", regs->r8);
+    printf("RAX: 0x%llx\n", regs->rax);
+    printf("RCX: 0x%llx\n", regs->rcx);
+    printf("RDX: 0x%llx\n", regs->rdx);
+    printf("RSI: 0x%llx\n", regs->rsi);
+    printf("RDI: 0x%llx\n", regs->rdi);
+    printf("ORIG_RAX: 0x%llx\n", regs->orig_rax);
+    printf("RIP: 0x%llx\n", regs->rip);
+    printf("CS: 0x%llx\n", regs->cs);
+    printf("EFLAGS: 0x%llx\n", regs->eflags);
+    printf("RSP: 0x%llx\n", regs->rsp);
+    printf("SS: 0x%llx\n", regs->ss);
+    printf("FS_BASE: 0x%llx\n", regs->fs_base);
+    printf("GS_BASE: 0x%llx\n", regs->gs_base);
+    printf("DS: 0x%llx\n", regs->ds);
+    printf("ES: 0x%llx\n", regs->es);
+    printf("FS: 0x%llx\n", regs->fs);
+    printf("GS: 0x%llx\n", regs->gs);
 }
 
 void store_register(pid_t pid) {
@@ -125,11 +156,29 @@ void store_register(pid_t pid) {
         perror("ptrace GETREGS");
         exit(EXIT_FAILURE);
     }
+    print_registers(&regs);
 
     fwrite(&regs, sizeof(regs), 1, fd_r);
     fclose(fd_r);
 }
 
+void print_proc_maps(pid_t pid) {
+    char path[256];
+    snprintf(path, sizeof(path), "/proc/%d/maps", pid);
+
+    FILE *file = fopen(path, "r");
+    if (!file) {
+        perror("fopen");
+        return;
+    }
+
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file)) {
+        printf("%s", buffer);
+    }
+
+    fclose(file);
+}
 
 int main(int argc, char *argv[]) {
     // Received PID from command argument
@@ -138,13 +187,15 @@ int main(int argc, char *argv[]) {
     if (pid == -1) {
         return EXIT_FAILURE;
     }
- 
+    
     // Attach to checkpointee
     if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) == -1) {
         perror("ptrace PTRACE_ATTACH");
         return EXIT_FAILURE;
     }
     waitpid(pid, NULL, 0);
+
+    print_proc_maps(pid);
     
     // Clear dump files
     FILE* fd_m = fopen("memory_dump.bin", "w");
--- restore.c
+++ restore.c
@@ -53,6 +53,37 @@ void restore_memory(pid_t pid) {
     fclose(fd_l);
 }
 
+void print_registers(struct user_regs_struct *regs) {
+    printf("Registers:\n");
+    printf("R15: 0x%llx\n", regs->r15);
+    printf("R14: 0x%llx\n", regs->r14);
+    printf("R13: 0x%llx\n", regs->r13);
+    printf("R12: 0x%llx\n", regs->r12);
+    printf("RBP: 0x%llx\n", regs->rbp);
+    printf("RBX: 0x%llx\n", regs->rbx);
+    printf("R11: 0x%llx\n", regs->r11);
+    printf("R10: 0x%llx\n", regs->r10);
+    printf("R9: 0x%llx\n", regs->r9);
+    printf("R8: 0x%llx\n", regs->r8);
+    printf("RAX: 0x%llx\n", regs->rax);
+    printf("RCX: 0x%llx\n", regs->rcx);
+    printf("RDX: 0x%llx\n", regs->rdx);
+    printf("RSI: 0x%llx\n", regs->rsi);
+    printf("RDI: 0x%llx\n", regs->rdi);
+    printf("ORIG_RAX: 0x%llx\n", regs->orig_rax);
+    printf("RIP: 0x%llx\n", regs->rip);
+    printf("CS: 0x%llx\n", regs->cs);
+    printf("EFLAGS: 0x%llx\n", regs->eflags);
+    printf("RSP: 0x%llx\n", regs->rsp);
+    printf("SS: 0x%llx\n", regs->ss);
+    printf("FS_BASE: 0x%llx\n", regs->fs_base);
+    printf("GS_BASE: 0x%llx\n", regs->gs_base);
+    printf("DS: 0x%llx\n", regs->ds);
+    printf("ES: 0x%llx\n", regs->es);
+    printf("FS: 0x%llx\n", regs->fs);
+    printf("GS: 0x%llx\n", regs->gs);
+}
+
 void restore_register(pid_t pid) {
     FILE *fd_r = fopen("register_dump.bin", "r");
 
@@ -63,6 +94,7 @@ void restore_register(pid_t pid) {
 
     struct user_regs_struct regs;
     fread(&regs, sizeof(regs), 1, fd_r);
+    print_registers(&regs);
 
     if (ptrace(PTRACE_SETREGS, pid, NULL, &regs) == -1) {
         perror("ptrace SETREGS");
@@ -70,6 +102,24 @@ void restore_register(pid_t pid) {
     }
 
     fclose(fd_r);
+}
+
+void print_proc_maps(pid_t pid) {
+    char path[256];
+    snprintf(path, sizeof(path), "/proc/%d/maps", pid);
+
+    FILE *file = fopen(path, "r");
+    if (!file) {
+        perror("fopen");
+        return;
+    }
+
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file)) {
+        printf("%s", buffer);
+    }
+
+    fclose(file);
 }
 
 int main(int argc, char *argv[]) {
@@ -81,7 +131,8 @@ int main(int argc, char *argv[]) {
     }
 
     if (child_pid == 0) {
-        // In child process
+        setsid();
+
         FILE *fd_l = fopen("layout.bin", "r");
         if (!fd_l) {
             perror("Failed to open layout file");
@@ -126,6 +177,8 @@ int main(int argc, char *argv[]) {
         // // Restore checkpoint data
         restore_memory(child_pid);
         restore_register(child_pid);
+
+        print_proc_maps(child_pid);
 
         // // Detach from the new process
         if (ptrace(PTRACE_DETACH, child_pid, NULL, NULL) == -1) {
